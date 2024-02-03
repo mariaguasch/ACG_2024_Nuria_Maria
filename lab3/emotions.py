a16f321fcf4867dc9eab85bbb1841483e2bb8c43
Maria GT <92045209+mariaguasch@users.noreply.github.com>
2024-02-03 10:12:57 +0100

    emotions colours

--- lab3/emotions.py
+++ lab3/emotions.py
@@ -32,41 +32,44 @@ def plot_matrix(mat):
     ax.grid()
     plt.show()
 
-def plot_comparison(random, our):
-    num_iterations = 20
+def plot_comparison(random, our, num_iterations):
+    
     random_mae_values = np.array(random)
 
-    # Plotting the bars
     plt.bar(range(num_iterations), random_mae_values, label='MAE of Random Annotations')
 
-    # Drawing a horizontal line for mae_our_annotations
+    # horizontal line for our mae
     plt.axhline(y=our, color='r', linestyle='-', label='MAE of Our Annotations')
 
-    # Adding labels and title
     plt.xlabel('Iteration')
     plt.ylabel('Mean Absolute Error (MAE)')
     plt.title('Comparison of MAE between Our Annotations and Random Annotations')
     plt.legend()
-
-    # Show the plot
     plt.show()
 
 def plot_bases(bases):
-    plt.scatter(bases[:, 0], bases[:, 1], label='Data Points')
 
-    # Add labels and title
+    emotion_colors = {
+        'Angry': 'red',
+        'Boredom': 'grey',
+        'Disgust': 'green',
+        'Friendly': 'orange',
+        'Happiness':'yellow',
+        'Laughter':'fuchsia',
+        'Sadness':'blue',
+        'Surprise':'darkviolet'
+    }
+
+    for i, emotion in enumerate(emotion_colors.keys()):
+        plt.scatter(bases[i * 3:i * 3 + 3, 0], bases[i * 3:i * 3 + 3, 1], label=emotion, color=emotion_colors[emotion])
+
     plt.xlabel('Dimension 1')
     plt.ylabel('Dimension 2')
     plt.title('Scatter Plot of Data Points in Reduced Space')
-
-    # Show legend if needed
     plt.legend()
-
-    # Show the plot
     plt.show()
 
 
-dir_challenge = ""
 # matrices = loadmat(r'/Users/nuriacodina/Desktop/UPF/QUART/2N_TRIM/FGA/ACG_2024_Nuria_Maria/lab3/Lab_3/matrices.mat')
 matrices = loadmat("matrices.mat")
 
@@ -81,16 +84,15 @@ mae_our_annotations = np.mean(np.mean(np.abs(non_infinite_values)))
 
 # COMPARING CONSISTENCY WITH RANDOM ANNOTATIONS
 mae = []
-num_iterations = 20
+num_iterations = 30
 for i in range(num_iterations):
     random_mat = np.random.randint(0,9,(24,24))
     cons_sim_dif = random_mat - similarity_matrix 
     cons_sim_diff_values = cons_sim_dif[np.isfinite(consistency_matrix)]
     mae.append(np.mean(np.mean(np.abs(cons_sim_diff_values))))
 
-#print('mae of random', mae)
 
-plot_comparison(mae, mae_our_annotations)
+plot_comparison(mae, mae_our_annotations, num_iterations)
 
 #PLOT O EL QUE SIGUI DE RANDOM MAEs
 
@@ -102,17 +104,19 @@ plot_matrix(similarity_matrix)
 plot_matrix(diss_mat)
 
 #EXTRACT TWO BASES USING MULTIDIMENSIONAL SCALING
-A = -0.5 * (diss_mat ** 2) #Compute A
-#Compute necessary variables to calculate B
+
+A = -0.5 * (diss_mat ** 2)
+
+# necessary variables for B
 n = len(diss_mat)
 I_n = np.identity(n)
 J_n = np.ones((n, n))
 H = I_n - (1/n) * J_n
-B = np.dot(np.dot(H, A), H) #Compute the doubly-centered symmetric matrix B
 
-#Compute eigenvalues & eigenvectors of B
+B = np.dot(np.dot(H, A), H) 
+
 eigenvalues, eigenvectors = np.linalg.eig(B)
-# Sort eigenvalues and their corresponding eigenvectors
+
 sort_indices = np.argsort(eigenvalues)[::-1]
 eigenvalues = eigenvalues[sort_indices]
 eigenvectors = eigenvectors[:, sort_indices]
@@ -123,6 +127,5 @@ plot_bases(first_two_bases)
 #COMPROVACIÓ AMB FUNCIO JA CREADA
 mds = MDS(n_components=2, dissimilarity='precomputed')
 
-# Fit and transform the dissimilarity matrix
 mds_result = mds.fit_transform(diss_mat)
 plot_bases(mds_result)
