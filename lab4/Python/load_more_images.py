258233c46f9a6976d4fe58da64308f430f0c8054
Maria GT <92045209+mariaguasch@users.noreply.github.com>
2024-02-14 16:25:54 +0100

    Update load_more_images.py

--- lab4/Python/load_more_images.py
+++ lab4/Python/load_more_images.py
@@ -1,5 +1,19 @@
 from google_images_download import google_images_download
 import os
+import requests
+'''
+def internet_connection():
+    try:
+        response = requests.get("https://dns.tutorialspoint.com", timeout=5)
+        return True
+    except requests.ConnectionError:
+        return False    
+if internet_connection():
+    print("The Internet is connected.")
+else:
+    print("The Internet is not connected.")
+
+internet_connection
 
 def download_similar_images(seed_image_path, save_directory, num_images=10):
     # Create save directory if it doesn't exist
@@ -15,7 +29,12 @@ def download_similar_images(seed_image_path, save_directory, num_images=10):
     print(paths)
 
 # Example usage
-seed_image_path = "/Users/nuriacodina/Desktop/UPF/QUART/2N_TRIM/FGA/ACG_2024_Nuria_Maria/lab4/TRAINING/image_A0011.jpg"
-save_directory = "/Users/nuriacodina/Desktop/UPF/QUART/2N_TRIM/FGA/ACG_2024_Nuria_Maria/lab4/new_images"
+seed_image_path = "TRAINING/image_A0006.jpg"
+save_directory = "new_images"
 num_images = 5
-download_similar_images(seed_image_path, save_directory, num_images)
+download_similar_images(seed_image_path, save_directory, num_images)'''
+
+response = google_images_download.googleimagesdownload()   #class instantiation
+o_direct = 'new_training_images'
+arguments = {"limit":5, "output_directory":o_direct,'print_urls': True ,'similar_images':'TRAINING/image_A0006.jpg'}   #creating list of arguments
+paths = response.download(arguments) 
